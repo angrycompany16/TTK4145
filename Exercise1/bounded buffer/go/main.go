7ee538a91768986b9df846d40ad896ae24aaea56
angrycompany16 <dpersson16@gmail.com>
2025-01-08 14:21:42 +0100

    Task 3 - bounded buffer

--- Exercise1/bounded buffer/go/main.go
+++ Exercise1/bounded buffer/go/main.go
@@ -1,38 +1,34 @@
-
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-
-func producer(/*TODO: parameters?*/){
-
-    for i := 0; i < 10; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Printf("[producer]: pushing %d\n", i)
-        // TODO: push real value to buffer
-    }
-
+func producer(buf chan int) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("[producer]: pushing %d\n", i)
+		buf <- i
+		// TODO: push real value to buffer
+	}
 }
 
-func consumer(/*TODO: parameters?*/){
-
-    time.Sleep(1 * time.Second)
-    for {
-        i := 0 //TODO: get real value from buffer
-        fmt.Printf("[consumer]: %d\n", i)
-        time.Sleep(50 * time.Millisecond)
-    }
-    
+func consumer(buf chan int) {
+	time.Sleep(1 * time.Second)
+	for {
+		i := <-buf
+		fmt.Printf("[consumer]: %d\n", i)
+		time.Sleep(50 * time.Millisecond)
+	}
 }
 
+func main() {
+	// TODO: make a bounded buffer
+	buf := make(chan int, 5)
 
-func main(){
-    
-    // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
-    
-    select {}
+	go consumer(buf)
+	go producer(buf)
+
+	select {}
 }
